dd60035264db3894e5d1d4933c6a8d3b67c353ac
Pa Yeng Thao <payengt24@gmail.com>
2019-09-02 17:56:29 -0500

    Updates

---
+++ precache-manifest.bff8089ecc92a9526457011fe0cfdfd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7c781c6906a5187851400c0c4420a8",
+    "url": "/payengt24/clicky-game/index.html"
+  },
+  {
+    "revision": "b0b5f87370a7c2dc59ef",
+    "url": "/payengt24/clicky-game/static/css/main.5b25e34d.chunk.css"
+  },
+  {
+    "revision": "c781d8d14aa25daf4a98",
+    "url": "/payengt24/clicky-game/static/js/2.0fb7f9dc.chunk.js"
+  },
+  {
+    "revision": "b0b5f87370a7c2dc59ef",
+    "url": "/payengt24/clicky-game/static/js/main.eb5bc9e4.chunk.js"
+  },
+  {
+    "revision": "4956e3adb2a18a4e01ca",
+    "url": "/payengt24/clicky-game/static/js/runtime~main.63db9772.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/payengt24/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
